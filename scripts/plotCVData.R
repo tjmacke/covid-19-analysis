5dee6b01ce377c2e1d2c682bf40dbcc89688c2e3
Tom Macke <macke_tom@yahoo.com>
2020-07-10 09:49:23 -0700

    Add code to show basis of linear model

--- scripts/plotCVData.R
+++ scripts/plotCVData.R
@@ -18,17 +18,21 @@ plot(as.Date(cv$date, '%Y-%m-%d'), cv$confirmed, type='l', xlab='Date', ylab='Co
 lines(as.Date(cv$date, '%Y-%m-%d'), cv$daily_confirmed, lty=1, col='grey67')
 lines(as.Date(cv$date, '%Y-%m-%d'), cv$deaths, col='red')
 
-# linear model of daily confirmed vs date, last 15 days. I like this better than a moving average
+# daily confirmed (dc) and linear model of last 15 days fo ds vs date
+dc_pre <- cv[as.Date(cv$date, '%Y-%m-%d') <= as.Date(cv[nrow(cv)-14, 'date'], '%Y-%m-%d'),]
 dc <- cv[as.Date(cv$date, '%Y-%m-%d') >= as.Date(cv[nrow(cv)-14, 'date'], '%Y-%m-%d'),]
 dc_lm <- lm(dc$daily_confirmed ~ as.Date(dc$date, '%Y-%m-%d'))
-abline(dc_lm, col='grey67', lty=2)
+# Use 2 lines: first is dc not used in model, second is dc used in model
+lines(as.Date(dc_pre$date, '%Y-%m-%d'), dc_pre$daily_confirmed, lty=1, col='grey67')
+lines(as.Date(dc$date, '%Y-%m-%d'), dc$daily_confirmed, lty=1, lwd=3, col='grey67')
+abline(dc_lm, lty=2)
 
 # titles & legends
 title(main=paste(dataset, ' Covid-19 Counts Through ', cv$date[nrow(cv)], 'T23:59:59Z', sep=''))
 title(sub=paste('Source:', ifelse(cv$source[1] == 'world', src_world, src_states), sep=' '), cex=0.4)
 legend('topleft', inset=c(0.02, 0.02), bg='white',
-	legend=c('confirmed', 'daily confirmed', 'lm(dc ~ date, last 15 days)', 'deaths/confirmed', 'deaths'),
-		col=c('black', 'grey67', 'grey67', 'blue', 'red'), lty=c(1,1,2,1,1), cex=0.7)
+	legend=c('confirmed','daily confirmed used in model','daily confirmed not used int model','lm(dc ~ date, last 15 days)','deaths/confirmed','deaths'),
+		col=c('black','grey67','grey67','black','blue','red'), lwd=c(1,3,1,1,1,1), lty=c(1,1,1,3,1,1), cex=0.7)
 
 # plot the deaths/confirmed using scale on right side
 par(new=T)
--- scripts/plotCVData2.R
+++ scripts/plotCVData2.R
@@ -14,21 +14,25 @@ cv$d2c <- cv$deaths/cv$confirmed
 # put some space on the right side for 2nd axis
 par(mar=c(5, 5, 5, 5))
 
-# plot the various counts
+# plot the deaths
 plot(as.Date(cv$date, '%Y-%m-%d'), cv$deaths, type='l', col='red', xlab='Date', ylab='Deaths')
-lines(as.Date(cv$date, '%Y-%m-%d'), cv$daily_deaths, lty=1, col='orange')
 
-# linear model of daily deaths vs date, last 15 days. I like this better than a moving a average
-dd <- cv[as.Date(cv$date, '%Y-%m-%d') >= as.Date(cv[nrow(cv)-14,'date'], '%Y-%m-%d'),]
+# daily deaths (dd) and linear model of last 15 days of dd vs date.
+dd_pre <- cv[as.Date(cv$date, '%Y-%m-%d') <= as.Date(cv[nrow(cv)-14, 'date'], '%Y-%m-%d'),]
+dd <- cv[as.Date(cv$date, '%Y-%m-%d') >= as.Date(cv[nrow(cv)-14, 'date'], '%Y-%m-%d'),]
 dd_lm <- lm(dd$daily_deaths ~ as.Date(dd$date, '%Y-%m-%d'))
-abline(dd_lm, col='orange', lty=2)
+# Use 2 lines: first is dd not used in model (thin), second is  dd used in model (thick)
+lines(as.Date(dd_pre$date, '%Y-%m-%d'), dd_pre$daily_deaths, lty=1, col='orange')
+lines(as.Date(dd$date, '%Y-%m-%d'), dd$daily_deaths, lty=1, lwd=3, col='orange')
+# Now this works well, as the extended line makes the slope easier to see
+abline(dd_lm, lty=2)
 
 # titles & legends
 title(main=paste(dataset, ' Covid-19 Deaths Through ', cv$date[nrow(cv)], 'T23:59:59Z', sep=''))
 title(sub=paste('Source:', ifelse(cv$source[1] == 'world', src_world, src_states), sep=' '), cex=0.4)
 legend('topleft', inset=c(0.02, 0.02), bg='white',
-	legend=c('deaths', 'daily deaths', 'lm(dd ~ date, last 15 days)', 'deaths/confirmed'),
-		col=c('red', 'orange', 'orange', 'blue'), lty=c(1,1,2,1), cex=0.7)
+	legend=c('deaths', 'daily deaths used in model', 'daily deaths not used in model', 'lm(dd ~ date, last 15 days)', 'deaths/confirmed'),
+		col=c('red', 'orange', 'orange', 'black', 'blue'), lwd=c(1,3,1,1,1), lty=c(1,1,1,2,1), cex=0.7)
 
 # plot the deaths/confirmed using scale on right side
 par(new=T)
