e7e8a54259f2f68cf9602047bc695e80a8066620
Tom Macke <macke_tom@yahoo.com>
2020-07-21 08:53:43 -0700

    Simplify the plots; add est of IFR

--- scripts/plotCVData.R
+++ scripts/plotCVData.R
@@ -7,7 +7,6 @@ src_states <- 'https://covidtracking.com/api/v1/states/daily.csv'
 
 cv <- read.csv(fname, sep='\t')
 colnames(cv) <- c('date', 'region', 'country', 'lat', 'long', 'confirmed', 'deaths', 'recovered', 'source')
-cv$daily_confirmed <- c(cv$confirmed[1], diff(cv$confirmed))
 cv$d2c <- cv$deaths/cv$confirmed
 
 # put some space on the right side for 2nd axis
@@ -15,24 +14,15 @@ par(mar=c(5, 5, 5, 5))
 
 # plot the various counts
 plot(as.Date(cv$date, '%Y-%m-%d'), cv$confirmed, type='l', xlab='Date', ylab='Count')
-lines(as.Date(cv$date, '%Y-%m-%d'), cv$daily_confirmed, lty=1, col='grey67')
 lines(as.Date(cv$date, '%Y-%m-%d'), cv$deaths, col='red')
-
-# daily confirmed (dc) and linear model of last 15 days fo ds vs date
-dc_pre <- cv[as.Date(cv$date, '%Y-%m-%d') <= as.Date(cv[nrow(cv)-14, 'date'], '%Y-%m-%d'),]
-dc <- cv[as.Date(cv$date, '%Y-%m-%d') >= as.Date(cv[nrow(cv)-14, 'date'], '%Y-%m-%d'),]
-dc_lm <- lm(dc$daily_confirmed ~ as.Date(dc$date, '%Y-%m-%d'))
-# Use 2 lines: first is dc not used in model, second is dc used in model
-lines(as.Date(dc_pre$date, '%Y-%m-%d'), dc_pre$daily_confirmed, lty=1, col='grey67')
-lines(as.Date(dc$date, '%Y-%m-%d'), dc$daily_confirmed, lty=1, lwd=3, col='grey67')
-abline(dc_lm, lty=2)
+lines(as.Date(cv$date, '%Y-%m-%d'), cv$recovered, col='green')
 
 # titles & legends
 title(main=paste(dataset, ' Covid-19 Counts Through ', cv$date[nrow(cv)], 'T23:59:59Z', sep=''))
 title(sub=paste('Source:', ifelse(cv$source[1] == 'world', src_world, src_states), sep=' '), cex=0.4)
 legend('topleft', inset=c(0.02, 0.02), bg='white',
-	legend=c('confirmed','daily confirmed used in model','daily confirmed not used int model','lm(dc ~ date, last 15 days)','deaths/confirmed','deaths'),
-		col=c('black','grey67','grey67','black','blue','red'), lwd=c(1,3,1,1,1,1), lty=c(1,1,1,3,1,1), cex=0.7)
+	legend=c('confirmed', 'deaths', 'recovered', 'deaths/confirmed'),
+		col=c('black', 'red', 'green', 'blue'), lwd=c(1, 1, 1, 1), cex=0.7)
 
 # plot the deaths/confirmed using scale on right side
 par(new=T)
---
+++ scripts/plotCVData3.R
@@ -0,0 +1,53 @@
+# expexts ds, fname must be set before calling
+#
+
+# sources:
+src_world <- 'https://github.com/CSSEGISandData/COVID-19'
+src_states <- 'https://covidtracking.com/api/v1/states/daily.csv'
+
+cv <- read.csv(fname, sep='\t')
+colnames(cv) <- c('date', 'region', 'country', 'lat', 'long', 'confirmed', 'deaths', 'recovered', 'source')
+cv$daily_confirmed <- c(cv$confirmed[1], diff(cv$confirmed))
+cv$daily_deaths <- c(cv$deaths[1], diff(cv$deaths))
+cv$d2c <- cv$deaths/cv$confirmed
+
+# put some space on the right side for 2nd axis
+par(mar=c(5, 5, 5, 5))
+
+# plot the deaths
+plot(as.Date(cv$date, '%Y-%m-%d'), cv$deaths, type='l', col='red', xlab='Date', ylab='Deaths')
+
+# daily deaths (dd) and linear model of last 15 days of dd vs date.
+dd_pre <- cv[as.Date(cv$date, '%Y-%m-%d') <= as.Date(cv[nrow(cv)-14, 'date'], '%Y-%m-%d'),]
+dd <- cv[as.Date(cv$date, '%Y-%m-%d') >= as.Date(cv[nrow(cv)-14, 'date'], '%Y-%m-%d'),]
+dd_lm <- lm(dd$daily_deaths ~ as.Date(dd$date, '%Y-%m-%d'))
+# Use 2 lines: first is dd not used in model (thin), second is  dd used in model (thick)
+lines(as.Date(dd_pre$date, '%Y-%m-%d'), dd_pre$daily_deaths, lty=1, col='orange')
+lines(as.Date(dd$date, '%Y-%m-%d'), dd$daily_deaths, lty=1, lwd=3, col='orange')
+# Now this works well, as the extended line makes the slope easier to see
+abline(dd_lm, lty=2)
+
+cv$ifr_14 <- NA
+# kludge, find a way to find 2nd maximum
+fr <- 100
+lr <- nrow(cv)
+for(i in fr:lr) {
+	cv[i, 'ifr_14'] <- cv[i, 'daily_deaths']/cv[i-14, 'daily_confirmed']
+}
+ifr_14_lm <- lm(cv$ifr_14 ~ as.Date(cv$date, '%Y-%m-%d'))
+
+# titles & legends
+title(main=paste(dataset, ' Covid-19 ?? Through ', cv$date[nrow(cv)], 'T23:59:59Z', sep=''))
+title(sub=paste('Source:', ifelse(cv$source[1] == 'world', src_world, src_states), sep=' '), cex=0.4)
+legend('topleft', inset=c(0.02, 0.02), bg='white',
+	legend=c('deaths', 'daily deaths used in model', 'daily deaths not used in model', 'lm(dd ~ date, last 15 days)', 'deaths/confirmed', 'IFR 14', 'ml(IFR 14 ~ date)'),
+		col=c('red', 'orange', 'orange', 'black', 'blue', 'pink','black'), lwd=c(1,3,1,1,1,1,1), lty=c(1,1,1,2,1,1,2), cex=0.7)
+
+# plot the deaths/confirmed using scale on right side
+par(new=T)
+plot(as.Date(cv$date, '%Y-%m-%d'), cv$d2c, type='l', lty=1, col='blue', axes=F, xlab=NA, ylab=NA)
+lines(as.Date(cv$date, '%Y-%m-%d'), cv$ifr_14, col='pink')
+abline(ifr_14_lm, lty=2)
+
+axis(side=4)
+mtext(side=4, line=2.5, 'IFR')
